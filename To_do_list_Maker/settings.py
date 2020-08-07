519899fa8c319a56089de7a4eb3c1c22678a4547
Mohammed Rakib <rafz.rakib@gmail.com>
2020-08-08 01:26:31 +0600

    Todo Maker completed without design

--- To_do_list_Maker/settings.py
+++ To_do_list_Maker/settings.py
@@ -115,8 +115,11 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
--- To_do_list_Maker/urls.py
+++ To_do_list_Maker/urls.py
@@ -15,7 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from todo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #Auth
+    path('signup/', views.signupuser,name='signupuser'),
+    path('logout/', views.logoutuser,name='logoutuser'),
+    path('login/', views.loginuser,name='loginuser'),
+
+    #Todo
+    path('current/', views.currenttodos,name='currenttodos'),
+    path('home/', views.home,name='home'),
+    path('create/', views.createtodo, name='createtodo'),
+    path('completed/', views.completedtodos, name='completedtodos'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+
 ]
